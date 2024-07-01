e48e18fbd43ed57e231d9c1f9bf3677ab17ee934
sherrenjie <jiesherren@gmail.com>
2024-06-30 20:52:29 -0400

    js

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 const form = document.getElementById('contact-form')
 const successMessage = document.getElementById('success-message')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const firstName = document.getElementById('first-name').value.trim()
+    const lastName = document.getElementById('last-name').value.trim()
+    const email = document.getElementById('email').value.trim()
+    const queryType = document.getElementById('input[name="query-type"]:checked')
+    const message = document.getElementById('message').value.trim()
+    const consent = document.getElementById('consent').checked
+
+})
